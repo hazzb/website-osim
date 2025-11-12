ffc5524234c6ee2e713eb1dcb9a0fd1091cb1132
dan <152837590+hazzb@users.noreply.github.com>
2025-11-12 16:29:21 +0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// src/App.jsx
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1>Halo OSIM!</h1>
+      <p>Website kita sedang dalam pengembangan.</p>
+    </div>
+  )
+}
+
+export default App
